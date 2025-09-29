61b12f93f8933625ab12e17be3582788a7374b7d
MATHEUS DE OLIVEIRA CAVALCANTE <99699238+M4TTHEW-OC@users.noreply.github.com>
2025-09-29 16:58:56 -0300

    Aula_13

--- Aula_13/analisador_numerico.c
+++ Aula_13/analisador_numerico.c
@@ -3,14 +3,14 @@
 
     int main (){
 
-        int valores [TAM],i = 0, numMaior, numMenor, numerosPares = 0, numerosImpares = 0, soma = 0;
-        float mediaAritmetica;
+        int valores [TAM],i = 0, numMaior, numMenor, numerosPares = 0, numerosImpares = 0, n, encontrar = 0;
+        float mediaAritmetica, soma = 0;
 
-        printf("Bem vindo ao ordenador Numerico\n");
+        printf("\nBem vindo ao ordenador Numerico\n");
 
         for (i  ; i < TAM; i++)
         {
-            printf("Insira o um numero por vez:");
+            printf("Insira o um numero por vez:\n");
             scanf("%d", &valores[i]);
 
             if (i == 0 || valores[i] > numMaior) numMaior = valores[i];
@@ -41,8 +41,27 @@
 
             printf("A quantidade de numeros pares foi: %d\n", numerosImpares);
 
-            printf("A media aritmetica %.2f %d %d", mediaAritmetica, soma, i);
+            printf("A media aritmetica %.2f \n", mediaAritmetica);
 
-    }
+            printf("Digite um numero para verificar se esta no vetor:");
+            scanf("%d", &n);
+
+        for ( i = 0; i < TAM ; i++){
+            if (valores[i] == n){
+                printf("O numero %d foi encontrado na posicao %d", n, i);
+                encontrar = 1;
+            }
+        }  
+            if (  !encontrar) {
+                printf("O numero %d nao foi encontrado no vetor\n", n);
+                
+            }
+            
+        
+        
+        
+        return 0;
+    
+        }
         
     
