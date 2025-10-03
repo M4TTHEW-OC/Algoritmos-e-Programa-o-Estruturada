ed891d067c55a10ef148410363e8933d8e6e3643
MATHEUS DE OLIVEIRA CAVALCANTE <99699238+M4TTHEW-OC@users.noreply.github.com>
2025-10-02 21:14:21 -0300

    aula_14

---
+++ Aula_14/string.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+    int main (){
+
+        char segundo_nome[30];
+          char primeiro_nome[30];
+
+
+
+        printf("Digite o seu primeiro nome:");
+        scanf("%s", primeiro_nome);
+
+        printf("Digite o seu segundo nome:");
+        scanf("%s", segundo_nome);
+
+
+        printf("O nome digitado foi %s", primeiro_nome);
+
+                printf("O nome digitado foi %s", segundo_nome);
+
+
+        return 0;
+        
+    }
